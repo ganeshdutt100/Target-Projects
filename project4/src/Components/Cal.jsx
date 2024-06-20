3dbae7fdb469ccb4553f20b8d5f156f417f1b0eb
Ganesh Dutt <gdutt2304@gmail.com>
2024-06-20 13:09:46 +0530

    Calculator

--- project4/src/Components/Cal.jsx
+++ project4/src/Components/Cal.jsx
@@ -1,8 +1,8 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 export const Cal = () => {
-const [input, setInput] = useState("");
- 
-         const buttonArray = [
+  const [input, setInput] = useState("");
+
+  const buttonArray = [
     "1",
     "2",
     "3",
@@ -13,7 +13,6 @@ const [input, setInput] = useState("");
     "8",
     "9",
     "0",
-
     "+",
     "-",
     "*",
@@ -22,33 +21,42 @@ const [input, setInput] = useState("");
     "c",
   ];
 
-  const handleButtonClick =(value) =>{
-    if(value === "="){
-      try{
-        setInput(eval(input).toSring());
+  const handleButtonClick = (value) => {
+    if (value === "=") {
+      try {
+        setInput(eval(input).toString());
+      } catch (error) {
+        setInput("Error");
       }
-      catch(error){
-        setInput("Error")
-      }
-    }
-    else if(value ==="c"){
+    } else if (value === "c") {
       setInput("");
-    }else{
+    } else {
       setInput(input + value);
     }
-
-  }
+  };
 
   return (
-    <div className=" bg-dark-subtle py-4 mx-auto mt-4 rounded" style={{ width: "300px" }}>
-      <input type="text" value={input} disabled className=" fs-4 rounded ms-3" />
+    <div
+      className=" bg-dark-subtle py-4 mx-auto mt-4 rounded"
+      style={{ width: "300px" }}
+    >
+      <input
+        type="text"
+        value={input}
+        disabled
+        className=" fs-4 rounded ms-3"
+      />
       <div className=" d-flex flex-wrap gap-4 px-4 pt-3 justify-content-around">
-        {buttonArray.map((surya,index) => (
-          <button key={index} className='btn btn-outline-dark py-2 px-3' onClick={()=>handleButtonClick(surya)} >{surya}</button>
+        {buttonArray.map((surya, index) => (
+          <button
+            key={index}
+            className="btn btn-outline-dark py-2 px-3"
+            onClick={() => handleButtonClick(surya)}
+          >
+            {surya}
+          </button>
         ))}
-        
       </div>
     </div>
   );
 };
-  
